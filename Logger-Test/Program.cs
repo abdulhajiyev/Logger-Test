4a0693d74ada4073f34f8a295e882c5de276c891
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-07-07 07:24:05 -0800

    Init

---
+++ Logger-Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Logger_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
