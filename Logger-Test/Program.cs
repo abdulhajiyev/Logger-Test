8147d3a7b3101ca44eca2bb88f6bd7400286307d
Abdul Hajiyev <abdulhajiyev@gmail.com>
2021-07-08 07:51:01 -0800

    Logger Test

--- Logger-Test/Program.cs
+++ Logger-Test/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Serilog;
+using Serilog.Events;
 
 namespace Logger_Test
 {
@@ -6,7 +8,14 @@ namespace Logger_Test
     {
         static void Main(string[] args)
         {
+            var log = new LoggerConfiguration()
+                .WriteTo.File("log.txt")
+                .CreateLogger();
             Console.WriteLine("Hello World!");
+            log.Information("Test");
+            log.Warning("Test");
+            log.Error("Test");
+            log.Fatal("Test");
         }
     }
 }
